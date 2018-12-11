cc7f06c0178d46cd546aedf60d7a7a5c9fdec7a6
Nayelly Quintanilla <nayellyq5@gmail.com>
2018-12-11 23:27:32 +0000

    day 1 Buzzfeed

--- js/script.js
+++ js/script.js
@@ -8,7 +8,22 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
+        
+        var question1answer = question1(q1result)
         var totalScore;
     });
 
 });
+function question1(answer){
+    if (answer === "hitting the books"){
+        return 1;
+    }else if(answer === "partying"){
+        return 3;
+    }else if(answer === "cleaning"){
+        return 5;
+    }else if(answer === "sleeping"){
+        return 7;
+    }else{answer === "going to a festival"
+        return 9;
+    }
+}
